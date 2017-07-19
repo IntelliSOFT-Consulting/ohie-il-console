867c683a32a6b1b8efc22d7ace3faf8155907a82
Marcin Kruszyński <mkruszynski@soldevelo.com>
2017-07-19 13:34:32 +0200

    #112: ILR_HOST variable removed

--- run.sh
+++ run.sh
@@ -5,14 +5,12 @@
 : ${TRUSTCHAIN=""}
 : ${CERT_LOCATION=/etc/nginx/certs/sedish.net.crt}
 : ${KEY_LOCATION=/etc/nginx/certs/sedish.net.key}
-: ${ILR_HOST=localhost}
 
 export SERVER_ADDRESS
 export IL_PORT
 export CERT_LOCATION
 export KEY_LOCATION
 export TRUSTCHAIN
-export ILR_HOST
 
 /utils/replace-vars /var/www/html/config/default.json
 /utils/replace-vars /etc/nginx/sites-available/default
